04133043c6e219626e7c1573dd0dd42f621e8e9c
ExposionHero <ludiemil@gmail.com>
2018-09-23 10:18:40 +0200

    initial commit

--- wget/Program.cs
+++ wget/Program.cs
@@ -3,13 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace wget
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            var wc = new WebClient();
+
+            if (args.Length == 1)
+            {
+                string ending = "";
+
+                string pathending = args[0].Substring(args[0].Length - 4, 4);
+                string[] pathcenterarray = args[0].Split('/');
+                string pathcenterguess = pathcenterarray[pathcenterarray.Length - 1];
+
+                //Console.WriteLine(pathending);
+                //Console.WriteLine(pathcenterguess);
+
+                if(pathcenterguess.Substring(pathcenterguess.Length - 4, 4) == pathending)
+                {
+                    ending = pathcenterguess;
+                }
+                else
+                {
+                    ending = pathcenterguess + pathending;
+                }
+
+                //Console.WriteLine(ending);
+
+                Uri url = new Uri(args[0]);
+                wc.DownloadFile(url, Environment.CurrentDirectory + "\\" + ending);
+                //Console.WriteLine(Environment.CurrentDirectory);
+            }
+            else
+            {
+                Console.WriteLine("Benutze: wget <DEINE URL>");
+            }
         }
     }
 }
